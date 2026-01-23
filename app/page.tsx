1b7f24613ff12d96d3323223db8f6277e9f63b0c
pablolospe <yosoypxl@gmail.com>
2026-01-23 16:23:19 -0300

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,15 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 bg-white dark:bg-black sm:items-start">
-     <h1>tufiesta.uy</h1>
+        {/* <h1>tufiesta.uy</h1> */}
+        <Image
+          className="dark:invert"
+          src="/logo.webp"
+          alt="tufiesta.uy logo"
+          width={200}
+          height={200}
+          priority
+        />
         <PushNotificationManager />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h2 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
