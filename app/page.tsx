755eeee91d5e808282a318a099a490fd82ae8e57
pablolospe <yosoypxl@gmail.com>
2026-01-23 15:59:49 -0300

    UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,13 @@ import PushNotificationManager from "./components/PushNotificationManager";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 bg-white dark:bg-black sm:items-start">
+     <h1>tufiesta.uy</h1>
         <PushNotificationManager />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            PWA
-          </h1>
+          <h2 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            PWA test
+          </h2>
         </div>
 
       </main>
